df43e7a4377993834381cca439a86954944fa959
rain1598 <guacamolek@yandex.com>
2020-08-29 14:35:00 -0400

    Incomplete multithreading implementation

--- src/main/java/mybot/Main.java
+++ src/main/java/mybot/Main.java
@@ -2,14 +2,13 @@ package mybot;
 import java.io.*;
 import java.util.*;
 import org.javacord.api.*;
-import org.javacord.api.entity.channel.Channel;
 import org.javacord.api.entity.message.Messageable;
 import org.javacord.api.entity.server.Server;
 public class Main {
 	static BufferedReader Token = new BufferedReader(new BufferedReader(new InputStreamReader(Thread.currentThread().getContextClassLoader().getResourceAsStream("Token.txt"))));
 	static BufferedReader read = new BufferedReader(new BufferedReader(new InputStreamReader(Thread.currentThread().getContextClassLoader().getResourceAsStream("redditinsultsnodupe.txt"))));
 	
-	static HashMap<Channel, Thread> threads = new HashMap<Channel, Thread>();
+	static HashMap<Messageable, Thread> threads = new HashMap<Messageable, Thread>();
 	static HashMap<Server, String> prefixes = new HashMap<Server, String>();
 	static StringBuilder al = new StringBuilder();
 	static StringBuilder sp = new StringBuilder();
@@ -24,71 +23,98 @@ public class Main {
 		System.out.println("Logged in!");
 		
 		api.addMessageCreateListener(event -> {
+			Messageable chan = event.getChannel();
 			String pre = prefix(event.getServer().get());
 			String message = event.getMessage().getContent().toLowerCase();
 			if(message.contains(pre)) {
 				//boolean candelete = true; //candelete is for hiding commands
 				if (message.contains(pre + "!insult")) {//reddit database insulter
 					try {
-						insult(event.getChannel());
+						chan.sendMessage(insult());
 					} catch (IOException e) {
-						event.getChannel().sendMessage("This is an IOException you dumb fuck");
+						chan.sendMessage("This is an IOException you dumb fuck");
 					}
 				}
 				else if (message.contains(pre + "!haram")) {//long arabic character spammer
-					event.getChannel().sendMessage(al.toString());
+					chan.sendMessage(al.toString());
 				}
 				else if (message.contains(pre + "!space")) {//hard ping
-					event.getChannel().sendMessage(sp.toString());
+					chan.sendMessage(sp.toString());
 				}
 				else if (message.contains(pre + "!stop")) {//stop 
-					
+					stop(chan);
 				}
-				else if (message.contains(pre + "!runharam")) {//multithreaded haram
+				else if(message.contains(pre + "!run")) {
+					multithread th = new multithread();
+					th.chan = chan;
+					if(threads.containsKey(chan)){
+						threads.get(chan).interrupt();
+						threads.remove(chan);
+					}
+					if (message.contains(pre + "!runharam")) {//multithreaded haram
 					
-				}
-				else if (message.contains(pre + "!runinsult")) {//multithreaded insult
-					
-				}
-				else if (message.contains(pre + "!runspace")) {//multithreaded space
-					
-				}
-				else if (message.contains(pre + "!runping")) {//multithreaded pinging
-					
+					}
+					else if (message.contains(pre + "!runinsult")) {//multithreaded insult
+						System.out.println("here");
+						try {
+							th.spam = insult();
+						} catch (IOException e) {}
+						threads.put(chan, new multithread());
+						threads.get(chan).start();
+					}
+					else if (message.contains(pre + "!runspace")) {//multithreaded space
+						
+					}
+					else if (message.contains(pre + "!runping")) {//multithreaded pinging
+						
+					}
 				}
 				else if (message.contains(pre + "!die")) {//debug, exits program
-					event.getChannel().sendMessage("Going Offline...");
+					chan.sendMessage("Going Offline...");
 					api.disconnect();
 					System.exit(0);
 				}
 				else if (message.contains(pre + "!help")) {//help
-					event.getChannel().sendMessage("pre + !changeprefix\nprefix + !haram\nprefix + !spaces\nprefix + !die");
+					chan.sendMessage("pre + !changeprefix\nprefix + !haram\nprefix + !spaces\nprefix + !die");
 				}
 				//change pre
 				else if (message.length() > pre.length()+14 && message.substring(0, pre.length()+13).equals((pre + "!changeprefix"))) {
 					pre = message.split(" ")[1];
-					prefixes.put(event.getServer().get(), pre);
-					event.getChannel().sendMessage("pre changed to: "+pre);
+					if(pre.equals("f")) {
+						prefixes.remove(event.getServer().get());
+						chan.sendMessage("prefix reset");
+					}
+					else {
+						prefixes.put(event.getServer().get(), pre);
+						chan.sendMessage("prefix changed to: "+pre);
+					}
 				}
 				//else candelete = false;
 				//if(candelete)event.deleteMessage();
 			}
 		});
 	}
-	static void insult(Channel chan) throws IOException{//Reddit insulter
+	static String insult() throws IOException{//Reddit insulter
 		int n = (int) (Math.random()*21795);
 		for(int i = 0; i < n; i++) read.readLine();
 		String re = read.readLine();
 		if(re.length() > 2000)re = re.substring(0, 1999);
-		((Messageable) chan).sendMessage(re);
+		return re;
 	}
-	static void stop(Channel chan) {
-		if(threads.containsKey(chan)) {
-			threads.get(chan).interrupt();
-		}
+	static void stop(Messageable chan) {//stops multithreading
+		if(threads.containsKey(chan)) threads.get(chan).interrupt();
 	}
 	static String prefix(Server serv) {
 		if(prefixes.containsKey(serv)) return prefixes.get(serv);
 		return "f";
+	}
+}
+class multithread extends Thread{
+	public String spam;
+	public Messageable chan;
+	public void run(){
+		System.out.println(chan);
+		System.out.println(spam);
+		while(true) chan.sendMessage(spam);
 	}
 }
