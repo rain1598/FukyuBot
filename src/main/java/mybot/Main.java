6974dbd0f11f5f3069a74f0e3cd52e8574edfd0f
rain1598 <guacamolek@yandex.com>
2020-08-30 15:19:37 -0400

    message reader overhaul

--- src/main/java/mybot/Main.java
+++ src/main/java/mybot/Main.java
@@ -6,76 +6,97 @@ import java.util.concurrent.TimeUnit;
 import org.javacord.api.*;
 import org.javacord.api.entity.message.Messageable;
 import org.javacord.api.entity.server.Server;
+import org.javacord.api.event.message.MessageCreateEvent;
 public class Main {
 	static ClassLoader load = Thread.currentThread().getContextClassLoader();
 	static BufferedReader Token = new BufferedReader(new InputStreamReader(load.getResourceAsStream("Token.txt")));
-	static BufferedReader rin = new BufferedReader(new InputStreamReader(Thread.currentThread().getContextClassLoader().getResourceAsStream("redditinsultsnodupe.txt")));
+	static BufferedReader rin = new BufferedReader(new InputStreamReader(load.getResourceAsStream("redditinsultsnodupe.txt")));
 	static BufferedReader copy = new BufferedReader(new InputStreamReader(load.getResourceAsStream("copypastas.txt")));
-	
 	static HashMap<Messageable, Thread> threads = new HashMap<Messageable, Thread>();
 	static HashMap<Server, String> prefixes = new HashMap<Server, String>();
 	static StringBuilder haram = new StringBuilder();
 	static StringBuilder space = new StringBuilder();
+	static String message;
+	static Messageable chan; 
+	static String pre;
+	static String plagueis;
+	static String sorry;
+	static String doctor;
+	static String kira;
+	static String pandemonika;
+	static MessageCreateEvent event;
 	
 	public static void main(String[] args) throws IOException {
 		space.append((char)8203);
 		for(int i = 0; i < 999; i++) space.append("\n");
 		space.append((char)8203);//spaces
 		for(int i = 0; i < 2000; i++) haram.append(((char)65021));//allah
-		String plagueis = copy.readLine();
-		String sorry = copy.readLine();
-		String doctor = copy.readLine();
-		String kira = copy.readLine();
-		String pandemonika = copy.readLine();
+		plagueis = copy.readLine();
+		sorry = copy.readLine();
+		doctor = copy.readLine();
+		kira = copy.readLine();
+		pandemonika = copy.readLine();
 		
 		DiscordApi api = new DiscordApiBuilder().setToken(Token.readLine()).login().join();
 		System.out.println("Logged in!");
 		
 		api.addMessageCreateListener(event -> {
-			Messageable chan = event.getChannel();
-			String pre = prefix(event.getServer().get());
-			String message = event.getMessage().getContent().toLowerCase();
-			if(message.contains(pre)) {
-				//boolean candelete = true; //candelete is for hiding commands
-				if (message.contains(pre + "!insult")) {//reddit database insulter
-					chan.sendMessage(insult());
-				}
-				else if (message.contains(pre + "!haram")) {//long arabic character spammer
-					chan.sendMessage(haram.toString());
-				}
-				else if (message.contains(pre + "!space")) {//hard ping
-					chan.sendMessage(space.toString());
-				}
-				else if (message.contains(pre + "!stop")) {//stop
-					stop(chan);
-				}
-				else if(message.contains(pre + "!m")) {
-					threading(message, chan, pre);
-				}
-				else if (message.contains(pre + "!die")) {//debug, exits program
-					chan.sendMessage("Going Offline...");
-					api.disconnect();
-					System.exit(0);
-				}
-				else if (message.contains(pre + "!help")) {//help
-					chan.sendMessage("pre + !changeprefix\nprefix + !haram\nprefix + !spaces\nprefix + !die");
-				}
-				//change pre
-				else if (message.length() > pre.length()+14 && message.substring(0, pre.length()+13).equals((pre + "!changeprefix"))) {
-					pre = message.split(" ")[1];
-					if(pre.equals("f")) {
-						prefixes.remove(event.getServer().get());
-						chan.sendMessage("prefix reset");
-					}
-					else {
-						prefixes.put(event.getServer().get(), pre);
-						chan.sendMessage("prefix changed to: "+pre);
-					}
-				}
-				//else candelete = false;
-				//if(candelete)event.deleteMessage();
+			String check = event.getMessageContent().substring(0, prefix(event.getServer().get()).length()+1);
+			if(check.equals(prefix(event.getServer().get())+"!")) {
+				readmanager(event);
+			}
+			else if(event.getMessageContent().contains("diexit")) {//debug, exits program
+				event.getChannel().sendMessage("Going Offline...");
+				api.disconnect();
+				System.exit(0);
 			}
 		});
+	}
+	static void readmanager(MessageCreateEvent eve){
+		String[] cmd = eve.getMessageContent().split(" ");
+		chan = eve.getChannel();
+		pre = prefix(eve.getServer().get());
+		message = cmd[1];
+		event = eve;
+		switch(cmd[0].charAt(pre.length()+1)){
+		case 'p':paste();break;
+		case 'm':threading();break;
+		case 'c':changeprefix();break;
+		}
+	}
+	static void paste(){
+		switch(message) {
+		case "insult":
+			chan.sendMessage(insult()); break;//reddit database insulter
+		case "stop" :
+			stop(chan); break; //stop treads
+		case "haram" :
+			chan.sendMessage(haram.toString()); break;
+		case "space" :
+			chan.sendMessage(space.toString()); break;
+		case "plagueis" :
+			chan.sendMessage(plagueis.toString()); break;
+		case "sorry" :
+			chan.sendMessage(sorry.toString()); break;
+		case "doctor" :
+			chan.sendMessage(doctor.toString()); break;
+		case "kira" :
+			chan.sendMessage(kira.toString()); break;
+		case "pandemonika" :
+			chan.sendMessage(pandemonika.toString()); break;
+		case "help" :
+			break;
+		}
+	}
+	static void changeprefix(){
+		if(message.equals("f")) {
+			prefixes.remove(event.getServer().get());
+			chan.sendMessage("prefix reset");
+		}
+		else {
+			prefixes.put(event.getServer().get(), message);
+			chan.sendMessage("prefix changed to: "+message);
+		}
 	}
 	static String insult(){//Reddit insulter
 		String re = "";
@@ -93,7 +114,7 @@ public class Main {
 			threads.remove(chan);
 		}
 	}
-	static void threading(String message, Messageable chan, String pre){
+	static void threading(){
 		stop(chan);
 		multithread.chan = chan;
 		if (message.contains(pre + "!mharam")) {//multithreaded haram
